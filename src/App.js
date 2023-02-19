f976aaa7f3cf35d070ed5c36b29f58f4cecd39da
nhanlong00 <trannhanlong9@gmail.com>
2023-02-19 16:08:50 +0700

    nhanlong

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout';
+import { Home, About, Contact } from './pages/index'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path='about' element={<About />}></Route>
+            <Route path='contact' element={<Contact />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
